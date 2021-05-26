9f139f59dacf9a593fc53cfb12bb99a72626a6f2
Ricardo Marimón <rmarimon@nimbox.com>
2021-05-26 15:14:16 -0400

    Cleanup swatch picker

--- src/components/pickers/SwatchPicker.stories.tsx
+++ src/components/pickers/SwatchPicker.stories.tsx
@@ -1,7 +1,10 @@
-import { SwatchPicker, SwatchPickerProps } from "./SwatchPicker";
+import { Input } from '../controls/Input';
+import { SwatchPicker, SwatchPickerProps } from './SwatchPicker';
+
 
 const definition = {
     title: 'Component/Picker/SwatchPicker',
+    name: 'asd',
     component: SwatchPicker,
     argTypes: {
         scale: { control: { type: 'select', options: ['xs', 'sm', 'base', 'lg'] } },
@@ -10,19 +13,25 @@ const definition = {
 };
 export default definition;
 
-export const Parameterized = ({ swatches, popperClassName }: SwatchPickerProps) => {
-
-    return <SwatchPicker defaultValue="#906090" swatches={swatches} popperClassName={popperClassName} />;
-
+export const Parameterized = ({ scale, swatches, error, popperClassName }: SwatchPickerProps) => {
+    return (
+        <div className="flex flex-row justify-center">
+            <div className="grid grid-cols-3 gap-4 w-2/3">
+                <Input type="text" defaultValue="before" />
+                <SwatchPicker scale={scale} swatches={swatches} defaultValue="#906090" error={error} popperClassName={popperClassName} />
+                <Input type="text" defaultValue="after" />
+            </div>
+        </div>
+    );
 };
 Parameterized.args = {
+    scale: 'base',
     swatches: [
         '#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e',
         '#16a085', '#27ae60', '#2980b9', '#8e44ad', '#2c3e50',
         '#f1c40f', '#e67e22', '#e74c3c', '#ecf0f1', '#95a5a6',
         '#f39c12', '#d35400', '#c0392b', '#bdc3c7', '#7f8c8d'
     ],
+    error: false,
     popperClassName: 'grid grid-cols-5'
 };
-
-
--- src/components/pickers/SwatchPicker.tsx
+++ src/components/pickers/SwatchPicker.tsx
@@ -1,34 +1,55 @@
-import classnames from "classnames";
-import React, { FC, useRef, useState } from "react";
-import { useOutsideClick } from "../../hooks/useOutsideClick";
-import { Input, InputProps } from "../controls/Input";
+import classnames from 'classnames';
+import React, { useImperativeHandle, useRef, useState } from 'react';
+import { useOutsideClick } from '../../hooks/useOutsideClick';
+import { Input, InputProps } from '../controls/Input';
 
 
 export interface SwatchPickerProps extends InputProps {
-
     swatches: string[];
-    popperClassName: string;
-
+    popperClassName?: string;
 }
 
-export const SwatchPicker: FC<SwatchPickerProps> = ({ swatches, popperClassName, ...props }) => {
+export const SwatchPicker = React.forwardRef<HTMLInputElement, SwatchPickerProps>(({ swatches, popperClassName, onFocus, onBlur, ...props }, ref) => {
 
     const [visible, setVisible] = useState(false);
     const [target, popper] = useOutsideClick<HTMLInputElement, HTMLDivElement>(() => setVisible(!visible));
+    useImperativeHandle(ref, () => target.current!);
+
+    function handleOnFocus(event: React.FocusEvent<HTMLInputElement>) {
+        if (onFocus) { onFocus(event); }
+        setVisible(true);
+    }
+
+    function handleOnBlur(event: React.FocusEvent<HTMLInputElement>) {
+        setVisible(false);
+        if (onBlur) { onBlur(event); }
+    }
+
+    function setValue(event: React.MouseEvent<HTMLDivElement, MouseEvent>, element: React.RefObject<HTMLInputElement>, swatch: string) {
+        event.preventDefault();
+        const inputSetter = Object?.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value')?.set;
+        if (inputSetter) {
+            inputSetter.call(element.current, swatch);
+            var inputEvent = new Event('input', { bubbles: true });
+            element.current!.dispatchEvent(inputEvent);
+        }
+    }
 
     return (
         <div className="relative inline-block w-full">
-            <input type="text" ref={target} {...props} onFocus={() => setVisible(true)} />
+            <Input type="text" ref={target as any} {...props} onFocus={handleOnFocus} onBlur={handleOnBlur} />
             {visible &&
                 <div ref={popper} className={classnames(
                     'absolute border border-control-border rounded',
-                    'bg-white w-full mt-2 cursor-pointer', popperClassName)}>
+                    'bg-white w-full mt-2 cursor-pointer',
+                    popperClassName
+                )}>
                     {swatches.map(s =>
-                        <div onClick={() => target.current!.value = s} style={{ backgroundColor: s }}>&nbsp;</div>
+                        <div onMouseDown={(e) => setValue(e, target, s)} style={{ backgroundColor: s }}>&nbsp;</div>
                     )}
                 </div>
             }
         </div>
     );
 
-};
+});
