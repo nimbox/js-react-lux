ea25f2524bff8db9be6e097b864e4d85b52f71dc
Ricardo Marimón <rmarimon@nimbox.com>
2021-05-26 15:13:32 -0400

    Format

--- src/components/Delay.tsx
+++ src/components/Delay.tsx
@@ -6,10 +6,13 @@ import { FC, useEffect, useState } from 'react';
 //
 
 export const Delay: FC<{ delay?: number }> = ({ delay = 250, children }) => {
+
     const [show, setShow] = useState(false);
     useEffect(() => {
         const timeout = setTimeout(() => setShow(true), delay);
         return () => clearTimeout(timeout);
     }, [delay, setShow]);
+
     return show ? children as React.ReactElement : null;
+    
 }
