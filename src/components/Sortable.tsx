4845e9bc91811d9e8ead2a264751c30b4b8d9689
Karla Alzuro <kalzuro@MacBook-Pro-de-Karla.local>
2021-06-02 22:35:33 -0400

    Updated Sortable

--- src/components/Sortable.tsx
+++ src/components/Sortable.tsx
@@ -1,9 +1,9 @@
 import classnames from 'classnames';
-import React, { FC, LegacyRef, ReactChild, ReactElement, useCallback, useEffect, useRef, useState } from 'react';
+import React, { FC, LegacyRef, ReactChild, ReactElement, ReactFragment, ReactPortal, useCallback, useEffect, useRef, useState } from 'react';
 import { useDrag, useDrop } from "react-dnd";
 import { v4 as uuidv4 } from 'uuid';
 import { DragIcon } from '../icons';
-import { ComponentScale, controlIconSmallMarginPositiveSize } from './ComponentScale';
+import { ComponentScale, controlIconSmallMarginSize } from './ComponentScale';
 
 
 export interface SortableProps {
@@ -15,18 +15,17 @@ export interface SortableProps {
 interface SortableItemProps {
     scale?: ComponentScale;
     type: string;
-    findItem: (id: string) => { index: number };
+    findItem: (value: React.Key) => { child: React.ReactChild | React.ReactFragment | React.ReactPortal, index: number };
     onChange: (value: React.Key, target: number) => void;
     onUpdate: (source: number, target: number) => void;
-    
+
 }
 
 export const Sortable: FC<SortableProps> = (({ onChange, scale = 'base', className, children }) => {
 
-    const [childrenArray, setChildrenArray] = useState<any>([]);
+    const [childrenArray, setChildrenArray] = useState<(React.ReactChild | React.ReactFragment | React.ReactPortal)[]>([]);
     useEffect(() => setChildrenArray(React.Children.toArray(children)), [setChildrenArray, children]);
     const [key] = useState(uuidv4());
-
     console.log('childrenArray', children, childrenArray);
 
     const findItem = useCallback(
@@ -65,13 +64,14 @@ export const Sortable: FC<SortableProps> = (({ onChange, scale = 'base', classNa
 
 const SortableItem: FC<SortableItemProps> = (({ type, onChange, findItem, onUpdate, scale = 'base', children }) => {
 
-    const [isDragging, isOver, ref, refPreview] = useDraggable((children as ReactElement).key, type, onChange, findItem, onUpdate);
+    const [isDragging, isOver, ref, refPreview] = useDraggable((children as ReactChild | ReactFragment | ReactPortal).key, type, onChange, findItem, onUpdate);
     const [, drop] = useDrop(() => ({ accept: type }));
+    const [hover, setHover] = useState(false);
 
     return (
-        <li ref={drop}>
+        <li ref={drop} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             <div ref={(refPreview as LegacyRef<HTMLDivElement> | undefined)} className={classnames(
-                'relative group',
+                'relative ',
                 { '-ml-3 pl-3': scale === 'xs', '-ml-4 pl-4': scale === 'sm', '-ml-5 pl-5': scale === 'base', '-ml-6 pl-6': scale === 'lg' })}>
                 <div className="">
                     {React.cloneElement(children as ReactElement, {
@@ -79,9 +79,9 @@ const SortableItem: FC<SortableItemProps> = (({ type, onChange, findItem, onUpda
                     })}
                 </div>
                 <div ref={(ref as LegacyRef<HTMLDivElement> | undefined)} className={classnames(
-                    'absolute invisible group-hover:visible top-0 left-0 cursor-move')} >
-                    {!isDragging && <DragIcon className={classnames(
-                        controlIconSmallMarginPositiveSize[scale || 'base'],
+                    'absolute  top-1/2 left-0 cursor-move')} >
+                    {(hover && !isDragging) && <DragIcon className={classnames(
+                        controlIconSmallMarginSize[scale || 'base'],
                         'stroke-current stroke-1')} />}
                 </div>
             </div>
@@ -90,9 +90,9 @@ const SortableItem: FC<SortableItemProps> = (({ type, onChange, findItem, onUpda
 });
 
 const useDraggable =
-    (value: any, type: string,
+    (value: React.Key, type: string,
         onChange: (value: React.Key, target: number) => void,
-        findItem: (id: string) => { index: number },
+        findItem: (value: React.Key) => { child: React.ReactChild | React.ReactFragment | React.ReactPortal, index: number },
         onUpdate: (source: number, target: number) => void) => {
 
         const ref = useRef(null);
