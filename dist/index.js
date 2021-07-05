c20d229fd71f9107b15ac2589769f4019212a6d2
Ricardo Marimón <rmarimon@nimbox.com>
2021-07-05 19:21:34 -0400

    Build

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 import { jsx, jsxs, Fragment } from 'react/jsx-runtime';
+import tinycolor from 'tinycolor2';
 import classnames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import _debounce from 'lodash/debounce';
 import React, { useEffect, createContext, useContext, useRef, useImperativeHandle, useState, useLayoutEffect, createRef, useCallback, useMemo } from 'react';
-import tinycolor from 'tinycolor2';
 import ReactDOM from 'react-dom';
 import { usePopper } from 'react-popper';
 import { v4 } from 'uuid';
@@ -89,6 +89,28 @@ function __spreadArray(to, from) {
         to[j] = from[i];
     return to;
 }
+
+/**
+ * Avatar. Representation of a user in the system.
+ */
+var Avatar = function (_a) {
+    var src = _a.src, initials = _a.initials, color = _a.color, backgroundColor = _a.backgroundColor;
+    var c = backgroundColor ? color : tinycolor(color).isDark() ? 'white' : 'black';
+    var bg = backgroundColor ? backgroundColor : color;
+    if (src) {
+        return (jsx("span", __assign({ className: "inline-flex flex-row justify-center content-center rounded-full overflow-hidden", style: {
+                width: '1.5em',
+                verticalAlign: '10%'
+            } }, { children: jsx("span", __assign({ className: "w-full bg-center bg-cover", style: { fontSize: '0.5em', backgroundImage: "url(" + src + ")" } }, { children: "\u00A0" }), void 0) }), void 0));
+    }
+    else {
+        return (jsx("span", __assign({ className: "inline-flex flex-row justify-center content-center rounded-full overflow-hidden", style: {
+                width: '1.5em',
+                verticalAlign: '10%',
+                color: c, backgroundColor: bg
+            } }, { children: jsx("span", __assign({ style: { fontSize: '0.5em' } }, { children: initials }), void 0) }), void 0));
+    }
+};
 
 function SvgAngleDownIcon(props) {
     return (jsx("svg", __assign({ xmlns: "http://www.w3.org/2000/svg", width: "1em", height: "1em", viewBox: "0 0 32 32", fill: "none", stroke: "currentColor", strokeWidth: 0.5, strokeLinecap: "round", strokeLinejoin: "round" }, props, { children: jsx("path", { className: "angle-down-icon_svg__st0", d: "M26 12l-10 8-10-8" }, void 0) }), void 0));
@@ -578,7 +600,7 @@ var Tag = function (_a) {
     var crossBackgroundColor = useMemo(function () { return tinycolor(backgroundColor).darken(5).toString(); }, [backgroundColor]);
     var crossBackgroundHoverColor = useMemo(function () { return tinycolor(crossBackgroundColor).darken(10).toString(); }, [crossBackgroundColor]);
     var _c = useState(crossBackgroundColor), hoverColor = _c[0], setHoverColor = _c[1];
-    return (jsxs("span", __assign({ className: 'inline-flex flex-row items-baseline max-w-full rounded rounded-full', style: { lineHeight: '1', paddingLeft: '0.25em', paddingTop: '0.125em', paddingRight: '0.5em', paddingBottom: '0.125em', color: color, backgroundColor: backgroundColor } }, { children: [jsx(Cross, { showCross: !!onDelete, onMouseEnter: function () { return setHoverColor(crossBackgroundHoverColor); }, onMouseLeave: function () { return setHoverColor(crossBackgroundColor); }, onClick: function () { return onDelete && onDelete(); }, crossColor: color, circleColor: onDelete ? hoverColor : crossBackgroundColor, className: "block flex-none self-center cursor-pointer", style: { marginRight: '0.125em' } }, void 0), jsx("span", __assign({ onClick: !onDelete ? onClick : undefined, className: classnames('block flex-1 max-w-full truncate', { 'hover:underline cursor-pointer': onClick && !onDelete }), style: { height: '1.2em', lineHeight: '1.2em' } }, { children: children }), void 0)] }), void 0));
+    return (jsxs("span", __assign({ className: 'inline-flex flex-row items-baseline max-w-full rounded-full', style: { lineHeight: '1', paddingLeft: '0.25em', paddingTop: '0.125em', paddingRight: '0.5em', paddingBottom: '0.125em', color: color, backgroundColor: backgroundColor } }, { children: [jsx(Cross, { showCross: !!onDelete, onMouseEnter: function () { return setHoverColor(crossBackgroundHoverColor); }, onMouseLeave: function () { return setHoverColor(crossBackgroundColor); }, onClick: function () { return onDelete && onDelete(); }, crossColor: color, circleColor: onDelete ? hoverColor : crossBackgroundColor, className: "block flex-none self-center cursor-pointer", style: { marginRight: '0.125em' } }, void 0), jsx("span", __assign({ onClick: !onDelete ? onClick : undefined, className: classnames('block flex-1 max-w-full truncate', { 'hover:underline cursor-pointer': onClick && !onDelete }), style: { height: '1.2em', lineHeight: '1.2em' } }, { children: children }), void 0)] }), void 0));
 };
 
 // constants
@@ -1334,5 +1356,5 @@ Panel.Item = function (_a) {
     return (jsx("div", __assign({ className: classnames('-px-3 pl-6 py-2 cursor-pointer', { 'bg-primary-500': active }, className) }, { children: children }), void 0));
 };
 
-export { Button, Card, CheckBox, Choose, ChooseFn, Context$4 as Context, Control, Cross, DatePicker, Delay, Header, Helium, Input, Loading, Main, MoreOptionsButton, Navigator, Panel, Postit, Radio, RadioBar, RoundButton, Select, SwatchPicker, Tabs, Tag, TagPicker, TextArea, TimePicker, Toast, ToastContainer, ToastContent, ToastProvider, Toggle, ViewportProvider, useOnOutsideClick, useToast, useViewport };
+export { Avatar, Button, Card, CheckBox, Choose, ChooseFn, Context$4 as Context, Control, Cross, DatePicker, Delay, Header, Helium, Input, Loading, Main, MoreOptionsButton, Navigator, Panel, Postit, Radio, RadioBar, RoundButton, Select, SwatchPicker, Tabs, Tag, TagPicker, TextArea, TimePicker, Toast, ToastContainer, ToastContent, ToastProvider, Toggle, ViewportProvider, useOnOutsideClick, useToast, useViewport };
 //# sourceMappingURL=index.js.map
