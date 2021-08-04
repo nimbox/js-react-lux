154e31d3200add1135ef6f9a23820ff36075d6ba
Ricardo Marimón <rmarimon@nimbox.com>
2021-08-04 15:09:55 -0400

    Build

--- dist/modules/kanban/Kanban.d.ts
+++ dist/modules/kanban/Kanban.d.ts
@@ -1,10 +1,11 @@
 import { FC } from "react";
-import { ConnectDragSource } from "react-dnd";
+import { ConnectDragSource, ConnectDropTarget } from "react-dnd";
 export interface KanbanContextProps {
-    isDragging: boolean;
-    setIsDragging: (isDragging: boolean) => void;
+    isActive: boolean;
+    setIsActive: (isActive: boolean) => void;
 }
 export declare const KanbanContext: import("react").Context<KanbanContextProps>;
 export declare const KanbanProvider: FC;
 export declare const useKanbanContext: () => KanbanContextProps;
+export declare const useColumn: (id: string) => [any, ConnectDropTarget];
 export declare const useCard: (id: string) => [any, ConnectDragSource];
