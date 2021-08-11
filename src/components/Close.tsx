8317256a182e87965471672735c749e27e5bad09
Ricardo Marimón <rmarimon@nimbox.com>
2021-08-11 13:38:30 -0400

    Close icon

--- src/components/Close.tsx
+++ src/components/Close.tsx
@@ -1,7 +1,6 @@
-import classNames from 'classnames';
+import classnames from 'classnames';
 import { FC } from 'react';
 import { CrossIcon } from '../icons';
-import classnames from 'classnames';
 
 
-export const Close: FC<{ onClick: () => void, className: string }> = ({ onClick, className }) => <CrossIcon onClick={onClick} className={classNames("stroke-2 cursor-pointer", className)} />;
+export const Close: FC<{ onClick: () => void, className: string }> = ({ onClick, className }) => <CrossIcon onClick={onClick} className={classnames("stroke-2 cursor-pointer", className)} />;
