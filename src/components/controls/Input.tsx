d1878d1ee3a55981ca8920e4c65201176e094c07
Ricardo Marimón <rmarimon@nimbox.com>
2021-08-21 16:52:50 -0400

    Refactor input to use ems

--- src/components/controls/Input.tsx
+++ src/components/controls/Input.tsx
@@ -1,28 +1,28 @@
 import classnames from 'classnames';
 import React, { useContext } from 'react';
-import { ComponentScale, controlScale, controlText } from '../ComponentScale';
 import { Context } from './Control';
 
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    scale?: ComponentScale;
     error?: boolean;
 }
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ scale, error, className, ...props }, ref) => {
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ error, className, ...props }, ref) => {
 
     const context = useContext(Context);
 
     return (
         <input {...props} ref={ref} className={classnames(
-            controlText[scale || context.scale || 'base'],
-            'block w-full rounded border border-control-border',
+            'block w-full',
+            'lux-control-font lux-control-padding',
+            'rounded border border-control-border',
             error || context.error ?
-                'border-danger-500 focus:border-danger-500 focus:ring focus:ring-danger-500' :
-                'focus:border-primary-500 focus:ring focus:ring-primary-500',
-            'focus:ring-opacity-50 focus:outline-none disabled:opacity-50',
+                'text-danger-500 border-danger-500 focus:border-danger-500 focus:ring-danger-500 placeholder-danger-500' :
+                'focus:border-primary-500 focus:ring-primary-500 placeholder-control-border',
+            'focus:ring focus:ring-opacity-50 focus:outline-none',
+            'placeholder-opacity-40',
+            'disabled:opacity-50',
             className)}
-            style={{ padding: '0.5em 0.75em 0.5em 0.75em' }}
         />
     );
 
