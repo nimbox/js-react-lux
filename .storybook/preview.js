f1e0fbeb458b4db0f913de9d837fa83da7008402
Ricardo Marimón <rmarimon@nimbox.com>
2021-08-21 16:51:17 -0400

    Add i18next functionality to all stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,33 @@
+import { useEffect } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../src/i18n';
 import '../src/index.css';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+
+export const globalTypes = {
+    locale: {
+        name: 'Locale',
+        description: 'Locale to use for i18next translations',
+        defaultValue: 'light',
+        toolbar: {
+            icon: 'flag',
+            items: ['en', 'es'],
+            showName: false,
+        }
+    }
+};
 
 export const decorators = [
-  (Story) => (
-    <div className="text-base">
-      <DndProvider backend={HTML5Backend}>
-        <Story />
-      </DndProvider>
-    </div>
-  ),
+    (Story, context) => {
+        useEffect(() => { i18n.changeLanguage(context.globals.locale); } , [context]);
+        return (
+            <I18nextProvider i18n={i18n} locale="en">
+                <DndProvider backend={HTML5Backend}>
+                    <Story {...context}/>
+                </DndProvider>
+            </I18nextProvider>
+        );
+    }
 ];
