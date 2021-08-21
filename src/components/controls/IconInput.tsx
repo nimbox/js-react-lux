9a129dbfa018948f562b84e3db0fc9f7ae83ac08
Ricardo Marimón <rmarimon@nimbox.com>
2021-08-21 16:54:48 -0400

    Format using ems

--- src/components/controls/IconInput.tsx
+++ src/components/controls/IconInput.tsx
@@ -1,22 +1,20 @@
 import { default as classnames, default as classNames } from 'classnames';
 import React from 'react';
-import { ComponentScale } from '../ComponentScale';
 import { Input } from './Input';
 
 
 export interface IconInputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     left?: React.ComponentType<any>;
     right?: React.ReactNode;
-    scale?: ComponentScale;
     error?: boolean;
     className?: string;
 }
 
-export const IconInput = React.forwardRef(({ left, right, scale, error, className, ...props }, ref) => {
+export const IconInput = React.forwardRef(({ left, right, error, className, ...props }, ref) => {
     return (
         <div className={classNames('relative',
             className)}>
-            <Input ref={ref} scale={scale} className={classnames({ 'pl-9': left, 'pr-9': right })}  {...props} />
+            <Input ref={ref} className={classnames({ 'pl-9': left, 'pr-9': right })}  {...props} />
             {left &&
                 <div className="absolute inset-y-0 left-0 flex flex-row justify-center items-center" style={{ width: '2em' }}>
                     {left}
