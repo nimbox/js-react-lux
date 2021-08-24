50ba69214a15dd36798285d047efe87f7787911c
Ricardo Marimón <rmarimon@nimbox.com>
2021-08-24 15:49:23 -0400

    Change to em units.

--- src/components/controls/TextArea.tsx
+++ src/components/controls/TextArea.tsx
@@ -1,28 +1,28 @@
 import classnames from 'classnames';
 import React, { useContext } from 'react';
-import { ComponentScale, controlScale, controlText } from '../ComponentScale';
 import { Context } from './Control';
 
 
 export interface TextAreaProps extends React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
-    scale?: ComponentScale;
     error?: boolean;
 }
 
-export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(({ scale, error, className, style, ...props }, ref) => {
+export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(({ error, className, ...props }, ref) => {
 
     const context = useContext(Context);
 
     return (
         <textarea {...props} ref={ref} className={classnames(
-            controlText[scale || context.scale || 'base'],
-            'block w-full rounded border border-control-border',
+            'block w-full',
+            'lux-control-font lux-control-padding',
+            'rounded border border-control-border',
             error || context.error ?
-                'border-danger-500 focus:border-danger-500 focus:ring focus:ring-danger-500' :
-                'focus:border-primary-500 focus:ring focus:ring-primary-500',
-            'focus:ring-opacity-50 focus:outline-none disabled:opacity-50',
+                'text-danger-500 border-danger-500 focus:border-danger-500 focus:ring-danger-500 placeholder-danger-500' :
+                'focus:border-primary-500 focus:ring-primary-500 placeholder-control-border',
+            'focus:ring focus:ring-opacity-50 focus:outline-none',
+            'placeholder-opacity-40',
+            'disabled:opacity-50',
             className)}
-            style={{ padding: '0.5em 0.75em 0.5em 0.75em', ...style }}
         />
     );
 
