2c50f975b24b66ceb8d38ae4fed04036e4f8294f
Ricardo Marimón <rmarimon@nimbox.com>
2021-09-18 14:37:02 -0400

    Formatting

--- src/components/Popper.tsx
+++ src/components/Popper.tsx
@@ -12,11 +12,11 @@ import { sameWidth } from '../utilities/sameWidth';
 
 export type PopperPlacement = Placement;
 
-export interface PopperProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+export interface PopperProps extends React.HTMLAttributes<HTMLDivElement> {
 
     reference: Element;
-    placement?: PopperPlacement;
 
+    placement?: PopperPlacement;
     withArrow?: boolean;
     withSameWidth?: boolean;
 
@@ -24,7 +24,27 @@ export interface PopperProps extends React.DetailedHTMLProps<React.HTMLAttribute
 
 }
 
-export const Popper = React.forwardRef<HTMLDivElement, PopperProps>(({ reference, placement = 'bottom-start', withArrow = false, withSameWidth = false, className, children, ...props }, ref) => {
+export const Popper = React.forwardRef<HTMLDivElement, PopperProps>((props, ref) => {
+
+    // properties
+
+    const { 
+        
+        reference, 
+
+        placement = 'bottom-start', 
+        withArrow = false, 
+        withSameWidth = false, 
+        
+        className, 
+        
+        children,
+
+        ...divProps 
+    
+    } = props;
+
+    // configuration
 
     const [popper, setPopper] = useState<HTMLDivElement | null>(null);
     const [arrow, setArrow] = useState<HTMLDivElement | null>(null);
@@ -39,8 +59,10 @@ export const Popper = React.forwardRef<HTMLDivElement, PopperProps>(({ reference
         ]
     });
 
+    // render
+
     return ReactDOM.createPortal(
-        <div ref={setPopper} {...attributes.popper} {...props} className={classnames('z-40 popper-element', className)} style={styles.popper}>
+        <div ref={setPopper} {...attributes.popper} {...divProps} className={classnames('z-40 popper-element', className)} style={styles.popper}>
             {children}
             {withArrow && <div ref={setArrow} className="popper-arrow" style={styles.arrow} />}
         </div>,
