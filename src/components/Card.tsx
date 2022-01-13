1f62ae18aff0fbf9e905d07d1cbbbabea8506472
Ricardo Marimón <rmarimon@nimbox.com>
2022-01-12 23:16:58 -0500

    Make card borders optional

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
 import classnames from 'classnames';
+import React, { FC } from 'react';
 
 
 //
@@ -7,23 +7,23 @@ import classnames from 'classnames';
 //
 
 interface CardComponent extends FC<{ className?: string }> {
-    Header: FC<{ className?: string }>,
+    Header: FC<{ noBorder?: boolean, className?: string }>,
     Body: FC<{ className?: string }>,
-    Footer: FC<{ className?: string }>
+    Footer: FC<{ noBorder?: boolean, className?: string }>
 }
 
 export const Card: CardComponent = ({ className, children }) => (
-    <div className={classnames('bg-content-fg border border-content-border rounded', className)}>{children}</div>
+    <div className={classnames('bg-content-fg border rounded', className)}>{children}</div>
 );
 
-Card.Header = ({ className, children }) => (
-    <div className={classnames('border-b border-content-border', className)}>{children}</div>
+Card.Header = ({ noBorder, className, children }) => (
+    <div className={classnames({ 'border-b': !noBorder }, className)}>{children}</div>
 );
 
 Card.Body = ({ className, children }) => (
     <div className={className}>{children}</div>
 );
 
-Card.Footer = ({ className, children }) => (
-    <div className={classnames('border-t border-content-border', className)}>{children}</div>
+Card.Footer = ({ noBorder, className, children }) => (
+    <div className={classnames({ 'border-t': !noBorder }, className)}>{children}</div>
 );
