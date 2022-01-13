7e2cab930232ce1f1f2d3a1f28fb948c211e7a51
Ricardo Marimón <rmarimon@nimbox.com>
2022-01-12 23:18:25 -0500

    Make card border optional

--- docs/components/Card.tsx
+++ docs/components/Card.tsx
@@ -7,23 +7,23 @@ import classnames from 'classnames';
 //
 
 interface CardComponent extends FC<{ className?: string }> {
-    Header: FC<{ className?: string }>,
+    Header: FC<{ noBorder?: boolean, className?: string }>,
     Body: FC<{ className?: string }>,
-    Footer: FC<{ className?: string }>
+    Footer: FC<{ noBorder?: boolean, className?: string }>
 }
 
 export const Card: CardComponent = ({ className, children }) => (
     <div className={classnames('bg-content-fg border border-content-border rounded', className)}>{children}</div>
 );
 
-Card.Header = ({ className, children }) => (
-    <div className={classnames('border-b border-content-border', className)}>{children}</div>
+Card.Header = ({ noBorder = false, className, children }) => (
+    <div className={classnames({ 'border-b border-content-border': !noBorder }, className)}>{children}</div>
 );
 
 Card.Body = ({ className, children }) => (
     <div className={className}>{children}</div>
 );
 
-Card.Footer = ({ className, children }) => (
-    <div className={classnames('border-t border-content-border', className)}>{children}</div>
+Card.Footer = ({ noBorder = false, className, children }) => (
+    <div className={classnames({ 'border-t border-content-border': !noBorder }, className)}>{children}</div>
 );
