069ad68ad3379fda886086c3ff0171f1023e412e
Ricardo Marimón <rmarimon@nimbox.com>
2022-01-30 15:47:00 -0500

    Test multi select values

---
+++ src/sandbox/ArrayInput.stories.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { action } from '@storybook/addon-actions';
+import React, { ChangeEvent, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Button, Wrapper } from '..';
+import { Input } from '../components/controls/Input';
+import { Tag } from '../components/Tag';
+
+
+export default {
+    title: 'Sandbox/ArrayInput',
+    parameters: { layout: 'centered' }
+};
+
+
+export const Default = () => {
+
+    const { register, setValue, handleSubmit, reset } = useForm<{ choices: string[] }>({ defaultValues: { choices: ['123'] } });
+    const onSubmit = (data: any) => console.log(data);
+
+    // const [value, setValue] = useState<any>(["asd"]);
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <select multiple {...register('choices')}>
+                    {/* <option value="asd">asd</option>
+                    <option value="123">asd</option>
+                    <option value="444">444</option> */}
+                </select>
+                <Button onClick={() => setValue('choices', ['asd', 'xxx'])}>setValue</Button>
+                <Button onClick={() => reset({ choices: ['90', '60']})}>reset</Button>
+                <Button type="submit">Submit</Button>
+            </form>
+        </div>
+    );
+
+};
