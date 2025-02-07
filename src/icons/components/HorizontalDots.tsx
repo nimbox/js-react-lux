95c191f13c66f7bb740460695b49ce2dea1b5747
Ricardo Marimón <rmarimon@nimbox.com>
2025-02-07 15:38:50 -0400

    Add vertical and horizontal dots

---
+++ src/icons/components/HorizontalDots.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgHorizontalDots = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    {...props}
+  >
+    <circle cx={8} cy={16} r={2} />
+    <circle cx={16} cy={16} r={2} />
+    <circle cx={24} cy={16} r={2} />
+  </svg>
+);
+export default SvgHorizontalDots;
