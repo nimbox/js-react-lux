66825ed44f3a4511438929168b4c72a85da7ffce
Ricardo Marimón <rmarimon@nimbox.com>
2025-02-23 09:40:03 -0400

    Hook to get dynamic element height. Eventually should extend to width

---
+++ src/hooks/useElementHeight.ts
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+
+
+export function useElementHeight<T extends HTMLElement>(initialHeight: number = 0) {
+
+    const ref = useRef<T>(null);
+    const [height, setHeight] = useState(initialHeight);
+
+    const updateHeight = useCallback(() => {
+        if (ref.current) {
+            setHeight(ref.current.clientHeight);
+        }
+    }, []);
+
+    useLayoutEffect(() => {
+        updateHeight();
+    }, [updateHeight]);
+
+    useEffect(() => {
+        window.addEventListener('resize', updateHeight);
+        return () => window.removeEventListener('resize', updateHeight);
+    }, [updateHeight]);
+
+    return { ref, height };
+
+}
