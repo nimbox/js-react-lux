1e1824d13a56dc037b142be71e7315524b35f30d
Ricardo Marimón <rmarimon@nimbox.com>
2025-08-17 17:21:59 -0400

    Refactor Toast component imports: separate useToast from ToastContext for improved modularity.

--- src/components/Toast.stories.tsx
+++ src/components/Toast.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { ToastProvider, useToast } from './Toast';
+import { ToastProvider } from './Toast';
+import { useToast } from './ToastContext';
 
 
 // Definition
