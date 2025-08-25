a5c19426a1133ed63f2053b1fccaa362329baa02
Ricardo Marimón <rmarimon@nimbox.com>
2025-08-25 18:57:59 -0400

    Refactor MessageReactions component to return null when there are no reactions, update package.json to specify CSS file for elegant styles, and remove unused style files for age, control, elegant, lux, postit, and tooltip.

--- src/modules/chat/message/slots/MessageReactions.tsx
+++ src/modules/chat/message/slots/MessageReactions.tsx
@@ -56,6 +56,10 @@ export function MessageReactions() {
 
     // Render
 
+    if (sorted.length === 0) {
+        return null;
+    }
+
     return (
         <>
 
@@ -95,34 +99,6 @@ export function MessageReactions() {
         </>
 
     );
-
-    // return (
-    //     <Popup
-    //         visible={visible}
-    //         placement={PLACEMENT[direction] || 'top'}
-    //         onChangeVisible={setVisible}
-    //         Component={() => <MessageReactionDetails onRemoveReaction={handleRemoveReaction} />}
-    //     >
-    //         <button
-    //             onClick={handleClick}
-    //             className={classNames('flex -mt-2 z-10', {
-    //                 'justify-self-end mr-3': direction === 'outbound',
-    //                 'justify-self-start ml-3': direction === 'inbound'
-    //             })}
-    //         >
-    //             <span
-    //                 className="bg-white rounded-full shadow-md flex items-center justify-center min-w-6 h-6 text-base border border-gray-200 p-3 gap-0.5"
-    //             >
-    //                 {sortedReactions.map(reaction => (
-    //                     <span key={reaction.emoji} className="mx-0.5 text-base">
-    //                         {reaction.emoji}
-    //                     </span>
-    //                 ))}
-    //                 <span className="ml-0.5 text-xs">{count}</span>
-    //             </span>
-    //         </button>
-    //     </Popup>
-    // );
 
 }
 
